1add9d8ce52bf5ace2c82a1df0209df58a40a87a
KKimWang <yichengwang@umass.edu>
2021-04-04 23:57:01 -0400

    yicheng_menu click event

---
+++ public/yicheng_menu.js
@@ -0,0 +1,22 @@
+ //declare variables
+ let $about = $("#about");
+ let $stat_calculator = $("#stat_calculator");
+ let $iv_calculator = $("#iv_calculator");
+ let $damage_calculator = $("#damage_calculator");
+ 
+
+
+
+
+$about.click(function() {
+    window.location.href = "https://quiet-beyond-13399.herokuapp.com/yichengwang";
+});
+$stat_calculator.click(function() {
+    window.location.href = "https://quiet-beyond-13399.herokuapp.com/xingyuzhu";
+});
+$iv_calculator.click(function() {
+    window.location.href = "https://quiet-beyond-13399.herokuapp.com/yiquanxiao";
+});
+$damage_calculator.click(function() {
+    window.location.href = "https://quiet-beyond-13399.herokuapp.com/weiqiwang";
+});
