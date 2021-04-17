13afbd9409c936f9689b0bebd4a415704a5723f3
KKimWang <yichengwang@umass.edu>
2021-04-17 11:10:52 -0400

    test db

--- index.js
+++ index.js
@@ -224,25 +224,7 @@ express()
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM pokemon');
       const results = { 'results': (result) ? result.rows : null};
-	  
-	  let pokemon = [];
-	  for( let i=0; i<results.length; i++ ) {
-	      let r = results[i];
-	      pokemon.push({ no: r.NO,
-	                    pokemon: r.pokemon,
-	                    hp: r.HP,
-						atk: r.Atk,
-						def: r.Def,
-						sp_atk: r.SpA,
-						sp_def: r.SpD,
-						speed: r.Spe,
-						total: r.Total,
-	                    type1: r.Type1,
-	                    type2: r.Type2});
-	  }
-	  
-	  
-      res.render('pages/db', {pokemon: pokemon} );
+      res.render('pages/db', results );
       client.release();
     } catch (err) {
       console.error(err);
@@ -254,24 +236,7 @@ express()
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM pokemon');
       const results = { 'results': (result) ? result.rows : null};
-      let pokemon = [];
-      for( let i=0; i<results.length; i++ ) {
-          let r = results[i];
-          pokemon.push({ no: r.NO,
-                        pokemon: r.pokemon,
-                        hp: r.HP,
-      						atk: r.Atk,
-      						def: r.Def,
-      						sp_atk: r.SpA,
-      						sp_def: r.SpD,
-      						speed: r.Spe,
-      						total: r.Total,
-                        type1: r.Type1,
-                        type2: r.Type2});
-      }
-      
-      
-      res.render('pages/yiquanxiao', {pokemon: pokemon} );
+      res.render('pages/yiquanxiao', results );
       client.release();
     } catch (err) {
       console.error(err);
