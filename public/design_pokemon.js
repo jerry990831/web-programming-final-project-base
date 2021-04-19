0fca083a09c355dd8a0bc4d9261fca15f8e75378
KKimWang <yichengwang@umass.edu>
2021-04-19 11:59:12 -0400

    test db

--- public/design_pokemon.js
+++ public/design_pokemon.js
@@ -85,17 +85,17 @@ $("#sub").click(function(e) {
 $(document).ready(function() {
 	 //add button event
 	 $("#addBtn").click(function() {
-		 // const pokemon = {
-			// 	NO: 25,
-			// 	Pokemon: $("#pokemon_name").val(),
-			// 	HP:$("#stats_hp").val(),
-			// 	Atk:$("#stats_atk").val(),
-			// 	Def:$("#stats_def").val(),
-			// 	SpA:$("#stats_sp_atk").val(),
-			// 	SpD:$("#stats_sp_def").val(),
-			// 	Spe:$("#stats_speed").val(),
-		 // };
-		 // console.log(pokemon);
+		 const pokemon = {
+				NO: 25,
+				Pokemon: $("#pokemon_name").val(),
+				HP:$("#stats_hp").val(),
+				Atk:$("#stats_atk").val(),
+				Def:$("#stats_def").val(),
+				SpA:$("#stats_sp_atk").val(),
+				SpD:$("#stats_sp_def").val(),
+				Spe:$("#stats_speed").val(),
+		 };
+		 console.log(pokemon);
 		 
 		 $.ajax({
 		 	url: "/pokemon",
