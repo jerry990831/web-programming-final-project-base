c4f6049fe09e8ba7cd176dfb28aebe39bca261ac
Xingyu Zhu <xingyuzhu@umass.edu>
2021-04-20 13:19:47 +0800

    'backend'

--- public/xingyuzhu.js
+++ public/xingyuzhu.js
@@ -100,17 +100,9 @@ function getAllSelect() {
             pokemons = data;
         }
     });
-    console.log(pokemons);
     pokemons.forEach(function(data) {
         $("#pokemon").append("<option>" + data.Pokemon + "</option>");
     });
-    
-    // $.get("/pokemon", function(pokemons) {
-    //     let $name = $("#pokemon");
-    //     pokemons.forEach(function(data) {
-    //         $name.append("<option>" + data.Pokemon + "</option>");
-    //     });
-    // });
 
     $.ajax({
         url: "/nature",
@@ -118,18 +110,12 @@ function getAllSelect() {
         dataType: "json", 
         async: false,
         success:function(data){
-            pokemons = data;
+            natures = data;
         }
     });
     natures.forEach(function(data) {
         $("#nature").append("<option>" + data.Nature + "</option>");
     });
-    // $.get("/nature", function(natures) {
-    //     let $nature = $("#nature");
-    //     natures.forEach(function(data) {
-    //         $nature.append("<option>" + data.Nature + "</option>");
-    //     });
-    // });
 }
 
 let hpS;
@@ -140,15 +126,6 @@ let spDS;
 let speS;
 $("#pokemon").change(showData);
 function showData() {
-    $.ajax({
-        url: "/pokemon",
-        type: "GET",
-        dataType: 'json', 
-        async: false,
-        success:function(data){
-            pokemons = data;
-        }
-    });
     pokemons.forEach(function(data) {
         if(data.Pokemon == $("#pokemon").val()){
             hpS = data.HP;
@@ -166,74 +143,75 @@ function showData() {
         }
     });
 }
-alert(pokemons);
-console.log(pokemons);
 
 $("#sub").click(statsCal);
-
+let hpW = 1;
+let atkW = 1;
+let defW = 1;
+let spAW = 1;
+let spDW = 1;
+let speW = 1;
 function statsCal() {
-    $.get("/nature", function(natures) {
-        let $nature = $("#nature");
-        natures.forEach(function(data) {
-            let hpW = 1;
-            let atkW = 1;
-            let defW = 1;
-            let spAW = 1;
-            let spDW = 1;
-            let speW = 1;
-            if(data.Nature == $("#nature").val()) {
-                let increaseS = data["Increased stat"];
-                let decreaseS = data["Decreased stat"];
-                if(increaseS == "Attack") {
-                    atkW = 1.1;
-                }else if(increaseS == "Defense") {
-                    defW = 1.1;
-                }else if(increaseS == "Sp. Attack") {
-                    spAW = 1.1;
-                }else if(increaseS == "Sp. Defense") {
-                    spDW = 1.1;
-                }else if(increaseS == "Speed") {
-                    speW = 1.1;
-                }
-
-                if(decreaseS == "Attack") {
-                    atkW = 0.9;
-                }else if(decreaseS == "Defense") {
-                    defW = 0.9;
-                }else if(decreaseS == "Sp. Attack") {
-                    spAW = 0.9;
-                }else if(decreaseS == "Sp. Defense") {
-                    spDW = 0.9;
-                }else if(decreaseS == "Speed") {
-                    speW = 0.9;
-                }
+    natures.forEach(function(data) {
+        if(data.Nature == $("#nature").val()) {
+            let increaseS = data["Increased stat"];
+            let decreaseS = data["Decreased stat"];
+            if(increaseS == "Attack") {
+                atkW = 1.1;
+            }else if(increaseS == "Defense") {
+                defW = 1.1;
+            }else if(increaseS == "Sp. Attack") {
+                spAW = 1.1;
+            }else if(increaseS == "Sp. Defense") {
+                spDW = 1.1;
+            }else if(increaseS == "Speed") {
+                speW = 1.1;
             }
-        });
+
+            if(decreaseS == "Attack") {
+                atkW = 0.9;
+            }else if(decreaseS == "Defense") {
+                defW = 0.9;
+            }else if(decreaseS == "Sp. Attack") {
+                spAW = 0.9;
+            }else if(decreaseS == "Sp. Defense") {
+                spDW = 0.9;
+            }else if(decreaseS == "Speed") {
+                speW = 0.9;
+            }
+        }
     });
 
-    var hp_input = $("#hp").val();
-    var ark_input = $("#atk").val();
-    var def_input = $("#def").val();
-    var spA_input = $("#spAtk").val();
-    var spD_input = $("#spDef").val();
-    var speed_input = $("#speed").val();
+    let hp_input = $("#hp").val();
+    let ark_input = $("#atk").val();
+    let def_input = $("#def").val();
+    let spA_input = $("#spAtk").val();
+    let spD_input = $("#spDef").val();
+    let spe_input = $("#speed").val();
 
-    $.get("/pokemon", function(pokemons) {
-        pokemons.forEach(function(data) {
-            if(data.Pokemon == $("#pokemon").val()){
-                lowerHP = calculateHP(data.HP, hp_input, 0);
-                upperHP = calculateHP(data.HP, hp_input, 31);
-                $("#hpF").text(lowerHP + " - " + upperHP);
+    lowerHP = calculateHP(hpS, hp_input, 0);
+    upperHP = calculateHP(hpS, hp_input, 31);
+    $("#hpF").text(lowerHP + " - " + upperHP);
 
-                $("#ATK").text(data.Atk);
-                $("#DEF").text(data.Def);
-                $("#sp_atk").text(data.SpA);
-                $("#sp_def").text(data.SpD);
-                $("#Speed").text(data.Spe);
-            }
-        });
-    });
+    lowerAtk = calculateOther(atkS, atkW, ark_input, 0);
+    upperAtk = calculateOther(atkS, atkW, ark_input, 31);
+    $("#atkF").text(lowerAtk + " - " + upperAtk);
+
+    lowerDef = calculateOther(defS, defW, def_input, 0);
+    upperDef = calculateOther(defS, defW, def_input, 31);
+    $("#defF").text(lowerDef + " - " + upperDef);
     
+    lowerSpA = calculateOther(spAS, spAW, spA_input, 0);
+    upperSpA = calculateOther(spAS, spAW, spA_input, 31);
+    $("#spAF").text(lowerSpA + " - " + upperSpA);
+
+    lowerSpD = calculateOther(spDS, spDW, spD_input, 0);
+    upperSpD = calculateOther(spDS, spDW, spD_input, 31);
+    $("#spDF").text(lowerSpD + " - " + upperSpD);
+
+    lowerSpe = calculateOther(speS, speW, spe_input, 0);
+    upperSpe = calculateOther(speS, speW, spe_input, 31);
+    $("#speF").text(lowerSpe + " - " + upperSpe);
 }
 
 function calculateHP(stat, base, iv) {
